b806e0fd4bee392930165943b26ed01a7726558c
Bogutskiy <bohutskiy2@gmail.com>
2022-07-16 14:18:52 +0300

    Update translations

--- src/web/js/index.js
+++ src/web/js/index.js
@@ -3,17 +3,15 @@ function initAnimations() {
     const text_block = $('.text-wrapper');
     const form_text = $('.form-text');
 
-    $(text_block).hide().fadeIn(2000);
-    $(form_block).hide().delay(4000).fadeIn(1500);
-    $(form_text).hide().delay(6000).fadeIn(1500);
+    $(text_block).hide().fadeIn(1500);
+    $(form_block).hide().delay(2000).fadeIn(1500);
+    $(form_text).hide().delay(3000).fadeIn(1500);
 }
 
 function initOnChange() {
-    const text_h = $('.text-wrapper h2');
-    const confirm_text = 'Process initialize...';
-
     $('#apply').change(() => {
-        text_h.text(confirm_text);
+        $('.app-text').toggle();
+        $('.process-text').toggle();
         setTimeout(() => $('form').submit(), 2000);
     });
 }
